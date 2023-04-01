b63445a98336b4525160ad0376eb740010abaf98
Queen Elisa V3 <125016907+xcocomd2000@users.noreply.github.com>
2023-04-01 20:29:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
-LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
+LANG: process.env.LANG === undefined ? 'SI' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
+FOOTER: process.env.FOOTER === undefined ? '💜⭕ 𝐁Ⓞⓣ Ỗ𝓕ℱιςＡŁ 💀⚠️ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=QvRFUSwQ#1n2vlncaUr08qph9SFzP-RNmjlrX5uXFC48HrmlrZ4M' : process.env.SESSION_ID
 };
